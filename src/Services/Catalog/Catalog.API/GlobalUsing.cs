d5758323d0899a25e8a6f67d2e0063eae89e9b48
Manuel Alfaro <manniealfaro@gmail.com>
2024-08-31 19:16:47 +0200

    Adds GetProducts feature with carter and mediatR
    - Implements get producs feature, with endpoint definition with carter
    - Uses mediatR to send the query and handle the functionality

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -3,3 +3,4 @@ global using BuildingBlocks.CQRS;
 global using Carter;
 global using Mapster;
 global using Marten;
+global using Catalog.API.Models;
---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,26 @@
+﻿namespace Catalog.API.Products.GetProducts;
+
+//public record GetProductsRequest();
+public record GetProductsResponse(IEnumerable<Product> Products);
+
+public class GetProductsEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            var query = new GetProductsQuery();
+
+            var result = await sender.Send(query);
+
+            var response = result.Adapt<GetProductsResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetProducts")
+        .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Gets all products")
+        .WithDescription("Gets all products in the catalog");
+    }
+}
