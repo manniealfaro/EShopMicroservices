f1857af52c51f68741e5ba543f3a02c8f12fc375
Manuel Alfaro <manniealfaro@gmail.com>
2024-09-03 18:37:54 +0200

    Implements command validator as a pipeline behaviour
    - Implements custom validators for commands with fluentAPI using abstract validator class
    - Use pipeline behaviours to run all validators for eveyr request
    - Implements Validation Behaviour as a generic IPipelineBehaviour so we share it accross al TCommands
    - Inject everything into DI

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -1,7 +1,9 @@
 ﻿global using MediatR;
 global using BuildingBlocks.CQRS;
+global using BuildingBlocks.Behaviours;
 global using Carter;
 global using Mapster;
 global using Marten;
 global using Catalog.API.Models;
 global using Catalog.API.Exceptions;
+global using FluentValidation;
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,19 +1,24 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container.
-builder.Services.AddCarter();
 
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+    config.AddOpenBehavior(typeof(ValidationBehaviour<,>));
 });
+
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+
+builder.Services.AddCarter();
 
 builder.Services.AddMarten(config =>
 {
     config.Connection(builder.Configuration.GetConnectionString("Database")!);
     config.AutoCreateSchemaObjects = Weasel.Core.AutoCreate.All; // Ajusta según tus necesidades
 }).UseLightweightSessions();
-
 
 var app = builder.Build();
 
